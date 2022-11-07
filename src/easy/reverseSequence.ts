46a8fd410ea01885c50b811c08306e949de50550
raulsanzz <88857186+raulsanzz@users.noreply.github.com>
2022-11-08 00:02:05 +0500

    Update reverseSequence.ts

--- src/easy/reverseSequence.ts
+++ src/easy/reverseSequence.ts
@@ -4,5 +4,10 @@
 // EXAMPLE:
 // n=5 //returns [5,4,3,2,1]
 export const reverseSeq = (n: number) => {
-  return false;
+  let arr = [];
+
+  for(let i = 1; i <= n; i++){
+    arr.push(i);
+  }
+  return arr.reverse()
 };
